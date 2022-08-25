59e8fa8abe327a2e22690b0df96f251ddb232a9f
Teus1998 <matheusmoscardi@hotmail.com>
2022-08-25 17:43:57 -0300

    dados1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,9 @@
-let Nome = Matheus
-let Email = Matheusmoscardi@hotmail.com
-let Profissão = Estudante 
-let Idade = 24 Anos
-let Viagem Dos Sonhos = Inglaterra
+let nome = "matheus"
+let email = "matheusmoscardi@hotmail.com"
+let profissao = "estudante"
+let idade = 24
+let viagemDosSonhos = "inglaterra"
+
+let mensagem = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + viagemDosSonhos + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+console.log(mensagem)
